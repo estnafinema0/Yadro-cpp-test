150e58b0eab6119a056678d5d687aebeb6375ad2
estnafinema0 <Kat9765830@gmail.com>
2025-02-08 00:04:41 +0300

    Implement Start method in LoadFile to

--- project/LoadFile.cpp
+++ project/LoadFile.cpp
@@ -1,4 +1,6 @@
 #include "LoadFile.hpp"
+#include "CheckConfigLines.hpp"
+#include "LoadFile.hpp"
 
 bool FileParser::readLines(const std::string &filename, std::string &errorLine) {
     FILE *file = std::fopen(filename.c_str(), "r");
@@ -26,4 +28,30 @@ FileParser::FileParser(const std::string &filename) {
     if (!readLines(filename, err)) {
         lines.push_back(err);
     }
+}
+
+bool FileParser::Start(ClubConfig &config, std::vector<EventData> &events, std::string & errorLine) {
+    int lineNum = 0;
+    if (lines.size() < 3){
+        errorLine = "Not enough configuration lines provided.\nMust be:\n<Number of Tables>\n<Opening Hours> <Closing Hours>\n<Hourly Cost>.";
+        return false;
+    }
+
+    if (!checkTableCount(lines[lineNum++], config, errorLine))
+        return false;
+    if (!checkOperatingHours(lines[lineNum++], config, errorLine))
+        return false;
+    if (!checkHourlyCost(lines[lineNum++], config, errorLine))
+        return false;
+
+    for (; lineNum < static_cast<int>(lines.size()); lineNum++) {
+        std::string line = lines[lineNum];
+        if (line.empty())
+            continue;
+        EventData event;
+        if (!checkEventLine(line, event, errorLine))
+            return false;
+        events.push_back(event);
+    }
+    return true;
 }
