4599c0ca92da468c06f577875570eb4b8a74c9ef
estnafinema0 <Kat9765830@gmail.com>
2025-02-07 22:29:45 +0300

    Implement ToString method in Time class

--- project/Time.cpp
+++ project/Time.cpp
@@ -1,4 +1,6 @@
 #include "Time.hpp"
+#include <iomanip>
+#include <sstream>
 
 int Time::FromString(const std::string &timeStr, bool &success){
     if (timeStr.size() != 5 || timeStr[2] != ':') {
@@ -30,5 +32,10 @@ int Time::FromString(const std::string &timeStr, bool &success){
 }
 
 std::string Time::ToString(int minutes){
-
+    int hours = minutes / 60;
+    int mins = minutes % 60;
+    std::ostringstream oss;
+    oss << std::setw(2) << std::setfill('0') << hours << ":"
+        << std::setw(2) << std::setfill('0') << mins;
+    return oss.str();
 }
