8b021ac062a34a5f29ef870b17a3a17aef1b205d
estnafinema0 <Kat9765830@gmail.com>
2025-02-08 21:21:46 +0300

    Add Club class

--- project/Club.hpp
+++ project/Club.hpp
@@ -4,13 +4,25 @@
 #include "Parser.hpp"
 
 struct Table {
-    int number;                 
+    int number;              // id number of the Table   
     bool occupied;         
     std::string currentClient; 
     int startTime;          
-    int totalOccupied;      // Total time occupied
-    int revenue;          
+    int totalOccupied;      // Total time occupied in minutes
+    int revenue;  
+
     Table(int num) : number(num), occupied(false), startTime(0), totalOccupied(0), revenue(0) {}
+};
+
+class Club {
+public:
+    Club(int tablesCount, int openTime, int closeTime, int hourlyCost);
+private:
+    int tablesCount;
+    int openTime;
+    int closeTime;
+    int hourlyCost;
+    std::vector<Table> tables;
 };
 
 #endif
