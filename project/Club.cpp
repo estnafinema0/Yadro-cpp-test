a66078839e432feb3fabfa213e2d4be22221f3fa
estnafinema0 <Kat9765830@gmail.com>
2025-02-08 23:00:43 +0300

    Add time conversion methods

--- project/Club.cpp
+++ project/Club.cpp
@@ -1,10 +1,18 @@
 #include "Club.hpp"
-
+#include "Time.hpp"
 
 
 Club::Club(int tablesCount, int openTime, int closeTime, int hourlyCost): tablesCount(tablesCount), openTime(openTime), closeTime(closeTime), hourlyCost(hourlyCost)
 {
     for (int i = 1; i <= tablesCount; i++)
         tables.push_back(Table(i));
+}
+
+std::string Club::getOpenTimeStr() const{
+    return Time::ToString(openTime);
+}
+
+std::string Club::getCloseTimeStr() const{
+    return Time::ToString(closeTime);
 }
 
--- project/Club.hpp
+++ project/Club.hpp
@@ -3,6 +3,9 @@
 
 #include "Parser.hpp"
 #include <vector>
+#include <set>
+#include <unordered_map>
+#include <string>
 
 struct Table {
     int number;              // id number of the Table   
@@ -18,14 +21,32 @@ struct Table {
 class Club {
 public:
     Club(int tablesnum, int openTime, int closeTime, int hourlyCost);
-
-    
+    // Process incoming event (from file).
+    void processEvent(const EventData &event);
+    // End of the day: freeing up remaining tables and generating event ID 11.
+    void endOfDay();
+    // Returns a list of strings to be printed to the console (in the correct order).
+    const std::vector<std::string>& getOutput() const;
+    // Generates and returns a final report on tables.
+    std::vector<std::string> getReport() const;
+    // Returns the opening time in the format "HH:MM".
+    std::string getOpenTimeStr() const;
+    // Returns the closing time in the format "HH:MM".
+    std::string getCloseTimeStr() const;
 private:
     int tablesCount;
     int openTime;
     int closeTime;
     int hourlyCost;
     std::vector<Table> tables;
+    std::set<std::string> currentClients;
+    std::unordered_map<std::string, int> clientsWithTables;
+    std::set<std::string> WaitingQueue;
+    std::vector<std::string> outputEvents;
+
+    void addOutputEvent(const std::string& event);
+
+    int computeRevenue(int minutes) const;
 };
 
 #endif
