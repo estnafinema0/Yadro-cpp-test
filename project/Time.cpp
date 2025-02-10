aaeb6d6844e9c48bd5b39e643a175293db39793e
estnafinema0 <Kat9765830@gmail.com>
2025-02-10 22:09:12 +0300

    Added another description to time

--- project/Time.cpp
+++ project/Time.cpp
@@ -30,12 +30,3 @@ int Time::FromString(const std::string &timeStr, bool &success){
     success = true;
     return static_cast<int>(hours * 60 + minutes);
 }
-
-std::string Time::ToString(int minutes){
-    int hours = minutes / 60;
-    int mins = minutes % 60;
-    std::ostringstream oss;
-    oss << std::setw(2) << std::setfill('0') << hours << ":"
-        << std::setw(2) << std::setfill('0') << mins;
-    return oss.str();
-}
--- project/Time.hpp
+++ project/Time.hpp
@@ -3,11 +3,18 @@
 
 #include <string>
 
-class Time {
-public:
-    static int FromString(const std::string &timeStr, bool &success);
+namespace Time {
+    int FromString(const std::string &timeStr, bool &success);
 
-    static std::string ToString(int minutes);
+    std::string ToString(int totalMinutes) {
+        int hours = totalMinutes / 60;
+        int minutes = totalMinutes % 60;
+        std::ostringstream oss;
+        oss << std::setw(2) << std::setfill('0') << hours << ":"
+            << std::setw(2) << std::setfill('0') << minutes;
+        return oss.str();
+    }
+
 };
 
 #endif
