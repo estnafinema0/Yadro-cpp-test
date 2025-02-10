f550fa228d22b9048bb4e2d4aa3ca8d78b614a05
estnafinema0 <Kat9765830@gmail.com>
2025-02-10 18:35:11 +0300

    Time.hpp structure was refined.

--- project/Time.hpp
+++ project/Time.hpp
@@ -1,13 +1,18 @@
-#ifndef TIME_HPP
-#define TIME_HPP
-
+#pragma once
+#include <string_view>
 #include <string>
+#include <optional>
+#include <sstream>
+#include <iomanip>
 
-class Time {
-public:
-    static int FromString(const std::string &timeStr, bool &success);
+namespace Time {
 
-    static std::string ToString(int minutes);
-};
+    // Parses the time in the "HH:MM" format from a string.
+    // If parsing is successful, returns the number of minutes since midnight,
+    // otherwise it returns std::nullptr.
+    inline std::optional<int> FromString(std::string_view timeStr);
 
-#endif
+    // Formats minutes in a string "HH:MM".
+    inline std::string ToString(int totalMinutes);
+
+} 
