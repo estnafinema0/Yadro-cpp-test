dc2f46609d9d5f4680f084d574e9b702d48760ef
estnafinema0 <Kat9765830@gmail.com>
2025-02-10 22:35:39 +0300

    Make up changes

--- project/Club.cpp
+++ project/Club.cpp
@@ -3,206 +3,183 @@
 #include <sstream>
 #include <algorithm>
 
+namespace Yadro {
 
-Club::Club(int tablesCount, int openTime, int closeTime, int hourlyCost): tablesCount(tablesCount), openTime(openTime), closeTime(closeTime), hourlyCost(hourlyCost)
-{
-    for (int i = 1; i <= tablesCount; i++)
-        tables.push_back(Table(i));
+Club::Club(int tablesnum, int openTime, int closeTime, int hourlyCost)
+    : m_tablesCount(tablesnum), m_openTime(openTime), m_closeTime(closeTime), m_hourlyCost(hourlyCost) {
+    for (int i = 1; i <= m_tablesCount; i++)
+        m_tables.push_back(Table(i));
 }
 
-std::string Club::getOpenTimeStr() const{
-    return Time::ToString(openTime);
+std::string Club::getOpenTimeStr() const {
+    return Time::ToString(m_openTime);
 }
 
-std::string Club::getCloseTimeStr() const{
-    return Time::ToString(closeTime);
-}
-
-void Club::addOutputEvent(const std::string& event){
-    outputEvents.push_back(event);
-}
-
-int Club::computeClientRevenue(int minutes) const{
-    return (minutes + 59) / 60 * hourlyCost;
-}
-
-void Club::processEvent(const EventData &event){
-
-    addOutputEvent(event.originalLine);
-
-    if (event.eventId == 1){
-        processEventID1(event.time, event.ClientName);
-    }
-    else if (event.eventId == 2){
-        processEventID2(event.time, event.ClientName, event.TableNumber);
-    }
-    else if (event.eventId == 3){
-        processEventID3(event.time, event.ClientName);
-    }
-    else if (event.eventId == 4){
-        processEventID4(event.time, event.ClientName);
-    }
-}
-
-void Club::processEventID1(int time, const std::string& client){
-    std::string clientName = client;
-    if (time < openTime || time > closeTime){
-        processErrorEvent(time, "NotOpenYet");
-        return;
-    }
-    if (currentClients.find(clientName) != currentClients.end()) {
-            processErrorEvent(time, "YouShallNotPass");
-            return;
-        }
-    currentClients.insert(clientName);
-}
-
-void Club::processEventID2(int time, const std::string& client, int tableNumber){
-    std::string clientName = client;
-
-     if (currentClients.find(clientName) == currentClients.end()) {
-            processErrorEvent(time, "ClientUnknown");
-            return;
-        }
-        if (tableNumber < 1 || tableNumber > tablesCount) {
-            processErrorEvent(time, "InvalidTableNumber");
-            return;
-        }
-        int tableIndex = tableNumber - 1;
-        if (tables[tableIndex].occupied) {
-            processErrorEvent(time, "PlaceIsBusy");
-            return;
-        }
-        // If client is already seated at another table, free it.
-        if (seatedClients.find(client) != seatedClients.end()) {
-            int oldTableNum = seatedClients[client];
-            int oldTableIndex = oldTableNum - 1;
-            freeTable(oldTableIndex, time);
-        }
-        // Assign client to selected table
-        tables[tableIndex].occupied = true;
-        tables[tableIndex].currentClient = client;
-        tables[tableIndex].startTime = time;
-        seatedClients[client] = tableNumber;
-}
-
-void Club::processEventID3(int time, const std::string& client){
-    std::string clientName = client;
-//DO WE NEED TO CHECK IF THE CLIENT IS ALREADY IN THE CLUB?
-
-        // if (currentClients.find(clientName) == currentClients.end()) {
-        //     processErrorEvent(time, "ClientUnknown");
-        //     return;
-        // }
-
-        bool freeExists = false;
-        for (auto &table : tables) {
-            if (!table.occupied) {
-                freeExists = true;
-                break;
-            }
-        }
-        if (freeExists) {
-            processErrorEvent(time, "ICanWaitNoLonger!");
-            return;
-        }
-        // If client is not in waiting queue, add him.
-        if (std::find(waitingQueue.begin(), waitingQueue.end(), client) == waitingQueue.end())
-            waitingQueue.push_back(client);
-        // If number of waiting clients is greater than number of tables, client leaves immediately.
-        if (static_cast<int>(waitingQueue.size()) > tablesCount) {
-            // remove-erase idiom
-            waitingQueue.erase(std::remove(waitingQueue.begin(), waitingQueue.end(), client), waitingQueue.end());
-            currentClients.erase(client);
-            seatedClients.erase(client);
-            std::ostringstream oss;
-            oss << Time::ToString(time) << " 11 " << client;
-            addOutputEvent(oss.str());
-        }
-}
-
-void Club::processEventID4(int time, const std::string& client){
-    std::string clientName = client;
-    if (currentClients.find(clientName) == currentClients.end()) {
-        processErrorEvent(time, "ClientUnknown");
-        return;
-    }
-    if (seatedClients.find(clientName) != seatedClients.end()) {
-        int tableNum = seatedClients[clientName];
-        int tableIndex = tableNum - 1;
-        freeTable(tableIndex, time);
-    }    
-    else {
-        waitingQueue.erase(std::remove(waitingQueue.begin(), waitingQueue.end(), clientName), waitingQueue.end());
-    }
-    currentClients.erase(clientName);
-}
-
-void Club::freeTable(int tableIndex, int eventTime){
-    if (tables[tableIndex].occupied) {
-        int duration = eventTime - tables[tableIndex].startTime;
-        if (duration < 0) duration = 0;
-        tables[tableIndex].totalOccupied += duration;
-        tables[tableIndex].revenue += computeClientRevenue(duration);
-        std::string client = tables[tableIndex].currentClient;
-        tables[tableIndex].occupied = false;
-        tables[tableIndex].currentClient = "";
-        seatedClients.erase(client);    
-
-        assignTableToWaiting(tableIndex, eventTime);
-    }
+std::string Club::getCloseTimeStr() const {
+    return Time::ToString(m_closeTime);
 }
 
 const std::vector<std::string>& Club::getOutput() const {
-    return outputEvents;
+    return m_outputEvents;
+}
+
+void Club::addOutputEvent(const std::string &event) {
+    m_outputEvents.push_back(event);
+}
+
+int Club::computeClientRevenue(int minutes) const {
+    return ((minutes + 59) / 60) * m_hourlyCost;
+}
+
+void Club::processEvent(const EventData &event) {
+    addOutputEvent(event.originalLine);
+    if (event.eventId == 1)
+        processEventID1(event.time, event.ClientName);
+    else if (event.eventId == 2)
+        processEventID2(event.time, event.ClientName, event.TableNumber);
+    else if (event.eventId == 3)
+        processEventID3(event.time, event.ClientName);
+    else if (event.eventId == 4)
+        processEventID4(event.time, event.ClientName);
+}
+
+void Club::processEventID1(int time, const std::string &client) {
+    if (time < m_openTime || time > m_closeTime) {
+        processErrorEvent(time, "NotOpenYet");
+        return;
+    }
+    if (m_currentClients.find(client) != m_currentClients.end()) {
+        processErrorEvent(time, "YouShallNotPass");
+        return;
+    }
+    m_currentClients.insert(client);
+}
+
+void Club::processEventID2(int time, const std::string &client, int tableNumber) {
+    if (m_currentClients.find(client) == m_currentClients.end()) {
+        processErrorEvent(time, "ClientUnknown");
+        return;
+    }
+    if (tableNumber < 1 || tableNumber > m_tablesCount) {
+        processErrorEvent(time, "InvalidTableNumber");
+        return;
+    }
+    int tableIndex = tableNumber - 1;
+    if (m_tables[tableIndex].occupied) {
+        processErrorEvent(time, "PlaceIsBusy");
+        return;
+    }
+    if (m_seatedClients.find(client) != m_seatedClients.end()) {
+        int oldTableNum = m_seatedClients[client];
+        int oldTableIndex = oldTableNum - 1;
+        freeTable(oldTableIndex, time);
+    }
+    m_tables[tableIndex].occupied = true;
+    m_tables[tableIndex].currentClient = client;
+    m_tables[tableIndex].startTime = time;
+    m_seatedClients[client] = tableNumber;
+}
+
+void Club::processEventID3(int time, const std::string &client) {
+    bool freeExists = false;
+    for (const auto &table : m_tables) {
+        if (!table.occupied) {
+            freeExists = true;
+            break;
+        }
+    }
+    if (freeExists) {
+        processErrorEvent(time, "ICanWaitNoLonger!");
+        return;
+    }
+    if (std::find(m_waitingQueue.begin(), m_waitingQueue.end(), client) == m_waitingQueue.end())
+        m_waitingQueue.push_back(client);
+    if (static_cast<int>(m_waitingQueue.size()) > m_tablesCount) {
+        m_waitingQueue.erase(std::remove(m_waitingQueue.begin(), m_waitingQueue.end(), client), m_waitingQueue.end());
+        m_currentClients.erase(client);
+        m_seatedClients.erase(client);
+        std::ostringstream oss;
+        oss << Time::ToString(time) << " 11 " << client;
+        addOutputEvent(oss.str());
+    }
+}
+
+void Club::processEventID4(int time, const std::string &client) {
+    if (m_currentClients.find(client) == m_currentClients.end()) {
+        processErrorEvent(time, "ClientUnknown");
+        return;
+    }
+    if (m_seatedClients.find(client) != m_seatedClients.end()) {
+        int tableNum = m_seatedClients[client];
+        int tableIndex = tableNum - 1;
+        freeTable(tableIndex, time);
+    } else {
+        m_waitingQueue.erase(std::remove(m_waitingQueue.begin(), m_waitingQueue.end(), client), m_waitingQueue.end());
+    }
+    m_currentClients.erase(client);
+}
+
+void Club::freeTable(int tableIndex, int eventTime) {
+    if (m_tables[tableIndex].occupied) {
+        int duration = eventTime - m_tables[tableIndex].startTime;
+        if (duration < 0)
+            duration = 0;
+        m_tables[tableIndex].totalOccupied += duration;
+        m_tables[tableIndex].revenue += computeClientRevenue(duration);
+        std::string client = m_tables[tableIndex].currentClient;
+        m_tables[tableIndex].occupied = false;
+        m_tables[tableIndex].currentClient = "";
+        m_seatedClients.erase(client);
+
+        if (eventTime != m_closeTime) {
+            assignTableToWaiting(tableIndex, eventTime);
+        }
+    }
 }
 
 void Club::assignTableToWaiting(int tableIndex, int eventTime) {
-    if (waitingQueue.empty())
+    if (m_waitingQueue.empty())
         return;
-    std::string client = waitingQueue.front();
-    waitingQueue.erase(waitingQueue.begin());
-
-    tables[tableIndex].occupied = true;
-    tables[tableIndex].currentClient = client;
-    tables[tableIndex].startTime = eventTime;
-    seatedClients[client] = tables[tableIndex].number;
-    // Generate event ID 12.
+    std::string client = m_waitingQueue.front();
+    m_waitingQueue.erase(m_waitingQueue.begin());
+    m_tables[tableIndex].occupied = true;
+    m_tables[tableIndex].currentClient = client;
+    m_tables[tableIndex].startTime = eventTime;
+    m_seatedClients[client] = m_tables[tableIndex].number;
     std::ostringstream oss;
-    oss << Time::ToString(eventTime) << " 12 " << client << " " << tables[tableIndex].number;
+    oss << Time::ToString(eventTime) << " 12 " << client << " " << m_tables[tableIndex].number;
     addOutputEvent(oss.str());
 }
 
 void Club::processErrorEvent(int time, const std::string &errorMsg) {
-   std::ostringstream oss;
-   oss << Time::ToString(time) << " 13 " << errorMsg;
-   addOutputEvent(oss.str());
+    std::ostringstream oss;
+    oss << Time::ToString(time) << " 13 " << errorMsg;
+    addOutputEvent(oss.str());
 }
 
 void Club::endOfDay() {
-    // For all occupied tables - compute time until closing.
-    for (auto &table : tables) {
-        if (table.occupied) {
-            freeTable(table.number - 1, closeTime);
-        }
+    for (auto &table : m_tables) {
+        if (table.occupied)
+            freeTable(table.number - 1, m_closeTime);
     }
-    // ID 11 in alphabetical order for seating + waiting clients.
-    std::vector<std::string> remainingClients(currentClients.begin(), currentClients.end());
+    std::vector<std::string> remainingClients(m_currentClients.begin(), m_currentClients.end());
     std::sort(remainingClients.begin(), remainingClients.end());
     for (const auto &client : remainingClients) {
         std::ostringstream oss;
-        oss << Time::ToString(closeTime) << " 11 " << client;
+        oss << Time::ToString(m_closeTime) << " 11 " << client;
         addOutputEvent(oss.str());
     }
 }
 
 std::vector<std::string> Club::getReport() const {
     std::vector<std::string> report;
-    for (const auto &table: tables) {
+    for (const auto &table : m_tables) {
         std::string occTime = Time::ToString(table.totalOccupied);
         std::ostringstream oss;
         oss << table.number << " " << table.revenue << " " << occTime;
         report.push_back(oss.str());
     }
     return report;
+}
+
 }
